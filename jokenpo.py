3a630bf7f14d76307818ebc2b80bb29aade9241b
Ramon-Venacio <ramonvenanciosousa@gmail.com>
2022-12-08 09:06:37 -0300

    Alteração

--- jokenpo.py
+++ jokenpo.py
@@ -1,17 +1,19 @@
 import random
 
-class Jokenpo:
-  plays = ["pedra","papel","tesoura"]
-  cpu = False
-  jogador = False
 
-  def partida(self):
-    while True:
-      print(f'+{"-"*15}+')
-      for i in range(3):
-        print(f'│\n')
-      
-      break
+class Jokenpo:
+    plays = ["pedra", "papel", "tesoura"]
+    cpu = False
+    jogador = False
+
+    def partida(self):
+        while True:
+            print(f'+{"-"*15}+')
+            for i in range(3):
+                print(f"│\n")
+
+            break
+
 
 jog1 = Jokenpo()
 jog1.partida()
